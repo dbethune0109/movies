8d659998529ef8072a1661a962ac1b1c0862ab5d
David Bethune <dbethune1970@gmail.com>
2016-11-13 14:17:32 -0800

    added valid ratings

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,20 +3,24 @@ import media
 # Toy Story info #
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
+                        "PG13",
                         "https://images-na.ssl-images-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg",
                         "https://www.youtube.com/watch?v=vwyZH85NQC4")
 # John Wick Info #
 john_wick = media.Movie("John Wick",
+                        "R",
                         "An ex-hitman comes out of retirement to track down the gangsters that took everything from him.",
                         "https://images-na.ssl-images-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_UX182_CR0,0,182,268_AL_.jpg",
                         "https://www.youtube.com/watch?v=2AUmvWm5ZDQ")
 # John Wick 2 Info #
 john_wick2 = media.Movie("John Wick 2",
+                         "R",
                         "Hitman John Wick is forced out of retirement by a former associate plotting to seize control of an international assassins' guild. Bound by a blood oath to help him, John travels to Rome where he squares off against the world's deadliest killers.",
                         "https://images-na.ssl-images-amazon.com/images/M/MV5BMTUxMzc4MTkwOF5BMl5BanBnXkFtZTgwOTE5MDcyMDI@._V1_UY268_CR3,0,182,268_AL_.jpg",
                         "https://www.youtube.com/watch?v=XGk2EfbD_Ps")
 # Star Wars A new hope info #
 star_wars = media.Movie("Star Wars",
+                        "PG13",
                         "Star Wars is an American epic space opera franchise, centered on a film series created by George Lucas. It depicts the adventures of various characters, a long time ago in a galaxy far, far away",
                         "https://images-na.ssl-images-amazon.com/images/M/MV5BZGEzZTExMDEtNjg4OC00NjQxLTk5NTUtNjRkNjA3MmYwZjg1XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg",
                         "https://www.youtube.com/watch?v=1g3_CFmnU7k")
