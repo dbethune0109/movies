3dabf694effcb9580101886ae92ffb03f071e350
David Bethune <dbethune1970@gmail.com>
2016-11-13 11:04:31 -0800

    fixed indent in line 4 and added valid_ratings to line 5 and line 7

--- media.py
+++ media.py
@@ -1,13 +1,14 @@
 import webbrowser
 
 class Movie():
-    """VALID_RATING G, PG, PG13,  R"""
-def __init__(udacity, movie_title, movie_storyline, poster_image, trailer_youtube):
+    """VALID_RATING G, PG, PG13,  R""" 
+def __init__(udacity, valid_rating,  movie_title, movie_storyline, poster_image, trailer_youtube):
         udacity.title = movie_title
+        udacity.valid_rating = VALID_RATING
         udacity.storyline = movie_storyline
         udacity.poster_image_url = poster_image
         udacity.trailer_youtube_url = trailer_youtube
-
+            
 def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
 print Movie. __doc__
