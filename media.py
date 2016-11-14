60d318eae8fa864f0e9375cc84d4ecd6010bcc12
David Bethune <dbethune1970@gmail.com>
2016-11-14 03:00:52 -0800

    fixed VALID_RATING

--- media.py
+++ media.py
@@ -2,13 +2,12 @@ import webbrowser
 
 class Movie():
     """VALID_RATING G, PG, PG13,  R""" 
-def __init__(udacity, valid_rating,  movie_title, movie_storyline, poster_image, trailer_youtube):
-        udacity.title = movie_title
-        udacity.valid_rating = VALID_RATING
-        udacity.storyline = movie_storyline
-        udacity.poster_image_url = poster_image
-        udacity.trailer_youtube_url = trailer_youtube
-            
+    def __init__(udacity,movie_title, movie_storyline, poster_image, trailer_youtube):
+            udacity.title = movie_title
+            udacity.storyline = movie_storyline
+            udacity.poster_image_url = poster_image
+            udacity.trailer_youtube_url = trailer_youtube
+print Movie. __doc__            
 def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
-print Movie. __doc__
+            webbrowser.open(self.trailer_youtube_url)
+
